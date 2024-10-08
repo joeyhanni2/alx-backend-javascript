ecef097db1e759fd89d25568543f17cb9392548e
joeyhanni2 <joeyhanni@gmail.com>
2024-10-08 22:36:35 +0000

    0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,13 @@
+function Student(id, firstName, location) {
+  this.id = id;
+  this.firstName = firstName;
+  this.location = location;
+}
+
+function getListStudents() {
+  return [
+    new Student(1, 'Guillaume', 'San Francisco'),
+    new Student(2, 'James', 'Columbia'),
+    new Student(5, 'Serena', 'San Francisco')
+  ];
+}
